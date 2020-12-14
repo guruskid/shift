100efe4831d43900059ff67558c902f455f949b7
slycreator <chinonso1amaechi@gmial.com>
2020-12-14 08:58:34 +0100

    move to cable

--- app/Http/Controllers/BillsPaymentController.php
+++ app/Http/Controllers/BillsPaymentController.php
@@ -75,23 +75,23 @@ class BillsPaymentController extends Controller
     }
 
 
-    public function getPackages(Request $r)
-    {
-        $client = new Client();
-        $url = env('RUBBIES_API') . "/billspaymentcustomerproducts";
-
-        $response = $client->request('POST', $url, [
-            'json' => [
-                "account" => $r->dec_num,
-                "servicename" => $r->biller,
-            ],
-            'headers' => [
-                'authorization' => env('RUBBIES_SECRET_KEY'),
-            ],
-        ]);
-        $body = json_decode($response->getBody()->getContents());
-        return response()->json($body);
-    }
+//    public function getPackages(Request $r)
+//    {
+//        $client = new Client();
+//        $url = env('RUBBIES_API') . "/billspaymentcustomerproducts";
+//
+//        $response = $client->request('POST', $url, [
+//            'json' => [
+//                "account" => $r->dec_num,
+//                "servicename" => $r->biller,
+//            ],
+//            'headers' => [
+//                'authorization' => env('RUBBIES_SECRET_KEY'),
+//            ],
+//        ]);
+//        $body = json_decode($response->getBody()->getContents());
+//        return response()->json($body);
+//    }
     public function CableView()
     {
 
@@ -101,98 +101,104 @@ class BillsPaymentController extends Controller
         $params['json'] = ["biller" => "cabletv",];
         $response = $client->post($url, $params);
         $body = json_decode($response->getBody()->getContents());
+
         if ($body->responsecode == "00"){
             $providers = $body->billers;
             //endpoint not provider yet
-            dd($providers);
-            return view('user.electricity', compact(['providers']));
+            // dd($providers);
+            return view('newpages.smartbudget', compact(['providers']));
         }
         else{
             return back()->with(['error' => 'Oops! ' . $body->responsemessage]);
         }
+
     }
     public function paytv(Request $r)
     {
         $r->validate([
-            'service_name' => 'required',
-            'package' => 'required',
+            'decoder' => 'required',
             'amount' => 'required',
             'password' => 'required',
+//            'productcode' => 'required',
+//            "billercode"  => 'required'
+
         ]);
 
+        $callback = route('recharge-card.callback');
         $n = Auth::user()->nairaWallet;
 
 //        if (Hash::check($r->password, $n->password) == false) {
 //            return redirect()->back()->with(['error' => 'Wrong wallet pin, please contact the support team if you forgot your pin']);
 //        }
-
+        dd('hi');
         $amount = $r->amount;
-        $reference = $r->ref;
+        //check if he has enough money
         if ($amount > $n->amount) {
             return redirect()->back()->with(['error' => 'Insufficient funds']);
         }
-
+        //what I remove here is below this page
+        $reference = $r->billercode .Str::random(16);
         $client = new Client();
-        $url = env('RUBBIES_API') . "/tvpurchase";
-        $response = $client->request('POST', $url, [
-            'json' => [
-                "reference" => $reference,
-                "bundleCode" => $r->package,
-                "amount" => $amount,
-                "phone" => Auth::user()->phone,
-                "name" => $r->name,
-                "servicename" => $r->service_name,
-                "smartcard" => $r->card_num
-            ],
-            'headers' => [
-                'authorization' => env('RUBBIES_SECRET_KEY'),
-            ],
+        $url = env('RUBBIES_API')."/billerpurchase";
+        $params['headers'] = ['Content-Type' => 'application/json', 'Authorization' => env('RUBBIES_SECRET_KEY')];
+        $params['json'] = [
+            "reference" => $reference,
+            "billercustomerid"  => $r->account,
+            "productcode"=> $r->productcode,
+            "amount" => $amount,
+            "mobilenumber" =>Auth::user()->phone,
+            "name" => Auth::user()->first_name,
+            "billercode" => $r->billercode,
+        ];
+        $response = $client->post($url,$params);
+        $body =  json_decode($response->getBody()->getContents());
+
+        $prev_bal = $n->amount;
+        $n->amount -= $amount;
+        $n->save();
+
+        $nt = new NairaTransaction();
+        $nt->reference = $reference;
+        $nt->amount = $amount;
+        $nt->user_id = Auth::user()->id;
+        //this is not a typo below,that was how the initial developer created it,unless you
+        //have access to change the db column name please dont change
+        $nt->type = 'elecriciy bills';
+
+        $nt->previous_balance = $prev_bal;
+        $nt->current_balance = $n->amount;
+        $nt->charge = (1/100) * $amount;
+        $nt->transaction_type_id = 11;
+
+
+        $nt->dr_user_id = Auth::user()->id;
+        $nt->dr_wallet_id = $n->id;
+        $nt->dr_acct_name = $n->account_name;
+        $nt->cr_acct_name = $r->provider;
+        $nt->narration = 'Payment for Cable Subscription bill';
+        $nt->trans_msg = 'done';
+        $nt->status = 'success';
+        $nt->save();
+
+        $title = 'Cable Subscription purchase';
+        $msg_body = 'Your Dantown wallet has been debited with N' . $amount . ' for cable subscription recharge';
+
+        $not = Notification::create([
+            'user_id' => Auth::user()->id,
+            'title' => $title,
+            'body' => $msg_body,
         ]);
-        $body = json_decode($response->getBody()->getContents());
-        if ($body->responsecode == 00) {
 
-            $prev_bal = $n->amount;
-            $n->amount -= $amount;
-            $n->save();
+        /* Mail::to(Auth::user()->email)->send(new DantownNotification($title, $msg_body)); */
 
-            $nt = new NairaTransaction();
-            $nt->reference = $reference;
-            $nt->amount = $amount;
-            $nt->user_id = Auth::user()->id;
-            $nt->type = 'paytv';
-
-            $nt->previous_balance = $prev_bal;
-            $nt->current_balance = $n->amount;
-            $nt->charge = (1.2/100) * $amount;
-            $nt->transaction_type_id = 12;
-
-
-            $nt->dr_user_id = Auth::user()->id;
-            $nt->dr_wallet_id = $n->id;
-            $nt->dr_acct_name = Auth::user()->first_name . ' ' . Auth::user()->last_name;
-            $nt->cr_acct_name = $r->service_name;
-            $nt->narration = 'Payment for cable subscription';
-            $nt->trans_msg = 'done';
-            $nt->save();
-
-            $title = 'Dantown wallet Debit';
-            $msg_body = 'Your Dantown wallet has been debited with N' . $amount . ' for cable subscription';
-
-            $not = Notification::create([
-                'user_id' => Auth::user()->id,
-                'title' => $title,
-                'body' => $msg_body,
-            ]);
-
-            /* Mail::to(Auth::user()->email)->send(new DantownNotification($title, $msg_body)); */
-
-            $token = env('SMS_TOKEN');
-            $to = Auth::user()->phone;
-            $sms_url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=' . $token . '&from=Dantown&to=' . $to . '&body=' . $msg_body . '&dnd=2';
-            $snd_sms = $client->request('GET', $sms_url);
-
-            return back()->with(['success' => 'Payment made successfully']);
+        $token = env('SMS_TOKEN');
+        $to = Auth::user()->phone;
+        //  $sms_url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=' . $token . '&from=Dantown&to=' . $to . '&body=' . $msg_body . '&dnd=2';
+        //   $snd_sms = $client->request('GET', $sms_url);
+        if ($body->responsecode == 00 || $body->responsecode == "-1") {
+            return back()->with(['success' => 'Purchase made successfully']);
         } else {
+
             return back()->with(['error' => 'Oops! ' . $body->responsemessage]);
         }
     }
@@ -200,7 +206,7 @@ class BillsPaymentController extends Controller
 
     public function airtime(Request $r)
     {
-        /* dd($r->all()); */
+
         $r->validate([
             'network' => 'required',
             'amount' => 'required',
@@ -328,7 +334,7 @@ class BillsPaymentController extends Controller
         $body = json_decode($response->getBody()->getContents());
         if ($body->responsecode == "00"){
             $providers = $body->billers;
-        return view('user.electricity', compact(['providers']));
+            return view('user.electricity', compact(['providers']));
         }
         else{
             return back()->with(['error' => 'Oops! ' . $body->responsemessage]);
@@ -347,6 +353,7 @@ class BillsPaymentController extends Controller
         ];
         $response = $client->post($url,$params);
         $body = json_decode($response->getBody()->getContents());
+
         if ($body != null ) {
             return response()->json($body->name);
         }
@@ -355,10 +362,10 @@ class BillsPaymentController extends Controller
 
     public function payElectricity(Request $r)
     {
-        dd('hi,this app works,');
+        // dd('hi,this app works,');
 
         $r->validate([
-            'scid' => 'required',
+            // 'scid' => 'required',
             'provider' => 'required',
             'account' => 'required',
             'amount' => 'required',
@@ -369,9 +376,9 @@ class BillsPaymentController extends Controller
         $callback = route('recharge-card.callback');
         $n = Auth::user()->nairaWallet;
 
-        if (Hash::check($r->password, $n->password) == false) {
-            return redirect()->back()->with(['error' => 'Wrong wallet pin, please contact the support team if you forgot your pin']);
-        }
+//        if (Hash::check($r->password, $n->password) == false) {
+//            return redirect()->back()->with(['error' => 'Wrong wallet pin, please contact the support team if you forgot your pin']);
+//        }
 
         $amount = $r->amount;
         //check if he has enough money
@@ -388,56 +395,56 @@ class BillsPaymentController extends Controller
             "billercustomerid"  => $r->account,
             "productcode"=> $r->productcode,
             "amount" => $amount,
-            "mobilenumber" =>Auth::user()->phone,
-//            "mobilenumber" => "08142381323",
+            // "mobilenumber" =>Auth::user()->phone,
+            "mobilenumber" => "08142381323",
             "name" => Auth::user()->first_name,
             "billercode" => $r->billercode,
         ];
         $response = $client->post($url,$params);
         $body =  json_decode($response->getBody()->getContents());
 
-            $prev_bal = $n->amount;
-            $n->amount -= $amount;
-            $n->save();
+        $prev_bal = $n->amount;
+        $n->amount -= $amount;
+        $n->save();
 
-            $nt = new NairaTransaction();
-            $nt->reference = $reference;
-            $nt->amount = $amount;
-            $nt->user_id = Auth::user()->id;
-            //this is not a typo below,that was how the initial developer created it,unless you
-            //have access to change the db column name please dont change
-            $nt->type = 'elecriciy bills';
+        $nt = new NairaTransaction();
+        $nt->reference = $reference;
+        $nt->amount = $amount;
+        $nt->user_id = Auth::user()->id;
+        //this is not a typo below,that was how the initial developer created it,unless you
+        //have access to change the db column name please dont change
+        $nt->type = 'elecriciy bills';
 
-            $nt->previous_balance = $prev_bal;
-            $nt->current_balance = $n->amount;
-            $nt->charge = (1/100) * $amount;
-            $nt->transaction_type_id = 11;
+        $nt->previous_balance = $prev_bal;
+        $nt->current_balance = $n->amount;
+        $nt->charge = (1/100) * $amount;
+        $nt->transaction_type_id = 11;
 
 
-            $nt->dr_user_id = Auth::user()->id;
-            $nt->dr_wallet_id = $n->id;
-            $nt->dr_acct_name = $n->account_name;
-            $nt->cr_acct_name = $r->provider;
-            $nt->narration = 'Payment for Electricity bill';
-            $nt->trans_msg = 'done';
-            $nt->status = 'success';
-            $nt->save();
+        $nt->dr_user_id = Auth::user()->id;
+        $nt->dr_wallet_id = $n->id;
+        $nt->dr_acct_name = $n->account_name;
+        $nt->cr_acct_name = $r->provider;
+        $nt->narration = 'Payment for Electricity bill';
+        $nt->trans_msg = 'done';
+        $nt->status = 'success';
+        $nt->save();
 
-            $title = 'Electricity purchase';
-            $msg_body = 'Your Dantown wallet has been debited with N' . $amount . ' for electricity recharge';
+        $title = 'Electricity purchase';
+        $msg_body = 'Your Dantown wallet has been debited with N' . $amount . ' for electricity recharge';
 
-            $not = Notification::create([
-                'user_id' => Auth::user()->id,
-                'title' => $title,
-                'body' => $msg_body,
-            ]);
+        $not = Notification::create([
+            'user_id' => Auth::user()->id,
+            'title' => $title,
+            'body' => $msg_body,
+        ]);
 
-            /* Mail::to(Auth::user()->email)->send(new DantownNotification($title, $msg_body)); */
+        /* Mail::to(Auth::user()->email)->send(new DantownNotification($title, $msg_body)); */
 
-            $token = env('SMS_TOKEN');
-            $to = Auth::user()->phone;
-            $sms_url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=' . $token . '&from=Dantown&to=' . $to . '&body=' . $msg_body . '&dnd=2';
-            $snd_sms = $client->request('GET', $sms_url);
+        $token = env('SMS_TOKEN');
+        $to = Auth::user()->phone;
+        //  $sms_url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=' . $token . '&from=Dantown&to=' . $to . '&body=' . $msg_body . '&dnd=2';
+        //   $snd_sms = $client->request('GET', $sms_url);
         if ($body->responsecode == 00 || $body->responsecode == "-1") {
             return back()->with(['success' => 'Purchase made successfully']);
         } else {
