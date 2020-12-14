617f155fde6f1540872dd51f243ed07850d9552f
slycreator <chinonso1amaechi@gmial.com>
2020-12-14 08:59:13 +0100

    move to cable

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -25,14 +25,14 @@ $(function () {
         $('#confirm-modal').modal('hide');
         $('#send-modal').modal('hide');
 
-     });
+    });
 
-     /* disable btn after buying airtime */
-     $('#airtime-form').submit(function (e) {
-         $('#recharge-btn').prop('disabled', true);
-         Notify('Transaction initiated, please wait', null, null, 'success');
-         $('#airtime-recharge-modal').modal('hide');
-      });
+    /* disable btn after buying airtime */
+    $('#airtime-form').submit(function (e) {
+        $('#recharge-btn').prop('disabled', true);
+        Notify('Transaction initiated, please wait', null, null, 'success');
+        $('#airtime-recharge-modal').modal('hide');
+    });
 
     /* Disable cable btn */
     $('#cable-form').submit(function (e) {
@@ -46,7 +46,7 @@ $(function () {
         $('#electricity-btn').prop('disabled', true);
         Notify('Transaction initiated, please wait', null, null, 'success');
         $('#electricity-modal').modal('hide');
-     });
+    });
 });
 /* Get the name of associated to the account number */
 function getAccountName() {
@@ -182,14 +182,11 @@ function showDetails(txnType, netwkPrvdr, phone, amount) {
 function getElectUser() {
     var providerId = $('#provider').find(':selected').data('scid');
     var account = $('#acct-num').val();
+    console.log(account)
     var productcode = $('#billercode').val();
     var acctName = $('#acct-name');
     if (productcode != '' && account != '') {
         acctName.val('Loading, please wait..');
-        var formData = {
-            account: account,
-            service_category_id: 2
-        };
         $('#d-provider').text($('#provider').find(':selected').text());
         $('#d-meter-no').text(account);
         $('#scid').val(providerId);
@@ -202,6 +199,7 @@ function getElectUser() {
                 "billercustomerid":account
             },
             success: function (result){
+                console.log('hi')
                 console.log(result)
                 if (result == undefined) {
                     acctName.val('No account found');
@@ -224,5 +222,33 @@ function getElectPrice() {
 function getAirtimeAmount() {
     var amount = $('#airtime_amount').val();
     $('#new-amount').text('₦' +(0.7 * amount));
+
+}
+function getCableUser() {
+    var productcode = $('#dec-billercode').val();
+    var decoder = $('#decoder').val();
+    var acctName = $('#owner-name');
+    if (productcode != '' && decoder != '') {
+        acctName.val('Loading, please wait..');
+        $.ajax({
+            type: "post",
+            url: "/user/get-elect-user",
+            dataType: 'json',
+            data: {
+                "billercode": productcode,
+                "billercustomerid":decoder
+            },
+            success: function (result){
+                if (result != undefined) {
+                    acctName.val(result);
+                    $('#owner-name').text(result);
+                }
+                // acctName.val('No account found');
+                // $('#owner-name').text('No account found');
+            }
+        });
+
+
+    }
 
 }
