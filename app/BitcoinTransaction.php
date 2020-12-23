9f2ff7bb89690be5853771956b7f0b3a1f87c4bd
sheanWinston <shenwinston@gmail.com>
2020-12-23 14:58:48 +0100

    bugs fixes

--- app/BitcoinTransaction.php
+++ app/BitcoinTransaction.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class BitcoinTransaction extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
