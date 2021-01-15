46d294baa820ccc27596973075372a309395067d
GODWIN JIMMY <57399112+Jimmygodwin@users.noreply.github.com>
2021-01-15 16:16:27 +0100

    Auto reload 419page to login page

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,56 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sorry Chief</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/stylesheet.css">
+<html>
+   <head>
+      <meta http-equiv = "refresh" content = "1; url = https://www.dantowntest.com.ng" />
+      <title>Sorry Chief</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+   </head>
 
-</head>
-<body>
-
-    <div class="" style="padding-top: 10%;">
-        <div class= style="padding-top: 10%; font-family: Quicksand;">
-
-            <div class="col-md-3 col-xs-12 col-sm-12" style="margin: auto; text-align:center; justify: ">
-                <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-                <lottie-player src="https://assets7.lottiefiles.com/private_files/lf30_yABbl9.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
-            </div>
-
-            <div style="margin: auto; text-align:center;">
-            <h1 style="font-family: Quicksand; color: #000070">
-                <strong>Sorry Chief</strong>
-            </h1>
-
-            <h3 style="font-family: Quicksand; color: #000070">
-                Something went wrong while loading this page
-            </h3>
-            <button style="padding-left: 2%; padding-right: 2%; border: 0px; background-color: #000070; color:white; " onclick="goBack()">Back</button>
-
-<script>
-function goBack() {
-  window.history.back();
-}
-</script>
-</div>
-
+   <body>
+       <div class="" style="overflow: hidden; margin-top: 100px; padding: 0px;">
+        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ObshHL.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px; margin: auto; text-align: center !important; justify-content: center !important;"  loop  autoplay></lottie-player>
+        <div class="" style="" >
+           <div class="" style="margin: auto; padding: 0px; text-align: center; font-family: Quicksand; color: #000070;">
+            <p style="font-size: 24px; padding-left: 5%; padding-right: 5%;">Sorry Chief, your session has expired please login to continue</p>
+           </div>
         </div>
+       </div>
 
+   </body>
 
-    </div>
-
-
-
-
-
-
-</head>
-<body>
-
-</body>
 </html>
