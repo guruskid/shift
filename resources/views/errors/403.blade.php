ca5337ae896414114895644c89160d57b8a703e9
GODWIN JIMMY <57399112+Jimmygodwin@users.noreply.github.com>
2021-01-15 17:00:46 +0100

    403 page update

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta http-equiv = "refresh" content = "1; url = https://www.dantowntest.com.ng" />
+      <title>Sorry Chief</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+   </head>
+
+   <body>
+       <div class="" style="overflow: hidden; margin-top: 100px; padding: 0px;">
+        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ObshHL.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px; margin: auto; text-align: center !important; justify-content: center !important;"  loop  autoplay></lottie-player>
+        <div class="" style="" >
+           <div class="" style="margin: auto; padding: 0px; text-align: center; font-family: Quicksand; color: #000070;">
+            <p style="font-size: 24px; padding-left: 5%; padding-right: 5%;">Sorry Chief, your session has expired please login to continue</p>
+           </div>
+        </div>
+       </div>
+
+   </body>
+
+</html>
