74ea7cf41a47da276e11ece606062958f8f707c9
sheanwinston <sheanwinston@gmail.com>
2021-02-23 12:28:54 +0100

    check

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -401,7 +401,7 @@ class AdminController extends Controller
     {
         $user = User::find($id);
         $transactions = $user->transactions;
-        dd($user);
+        dd($transactions);
 
         $wallet_txns = NairaTransaction::where('cr_user_id', $user->id)->orWhere('dr_user_id', $user->id)->orderBy('id', 'desc')->paginate(20);
         $dr_total = 0;
