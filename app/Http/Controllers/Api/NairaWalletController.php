e0cdb5e0c67f14f453a294144f7ffde28e940f7e
sheanwinston <sheanwinston@gmail.com>
2021-03-09 13:35:29 +0100

    New transfer

--- app/Http/Controllers/Api/NairaWalletController.php
+++ app/Http/Controllers/Api/NairaWalletController.php
@@ -65,7 +65,7 @@ class NairaWalletController extends Controller
         $n->password = Hash::make($r->new_pin);
         $n->save();
 
-        if (Auth::user()->bitcoinWallet->count() > 0) {
+        if (Auth::user()->bitcoinWallet) {
             Auth::user()->bitcoinWallet->password = Hash::make($r->new_pin);
             Auth::user()->bitcoinWallet->save();
         }
@@ -76,7 +76,7 @@ class NairaWalletController extends Controller
         ]);
     }
 
-    public function transfer(Request $r)
+    public function oldTransfer(Request $r)
     {
         $charge = 0;
         $bank_name = '';
@@ -301,5 +301,160 @@ class NairaWalletController extends Controller
                 'msg' => 'Oops! ' . $body->responsemessage
             ]);
         }
+    }
+
+    public function transfer(Request $r)
+    {
+        //Check If user owns a wallet
+        if (Auth::user()->accounts->count() == 0) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Please add account details to continue'
+            ]);
+        }
+
+        $validator = Validator::make($r->all(), [
+            'account_id' => 'required',
+            'pin' => 'required',
+            'amount' => 'required',
+            'narration' => 'nullable',
+            'ref' => 'required|unique:naira_transactions,reference',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 401);
+        }
+
+        $charge = 0;
+        $bank_name = '';
+        $acct_name = '';
+        $acct_num = '';
+        $bank_code = '';
+        $tid = 0;
+        $reference = $r->ref;
+
+        /* get bank details */
+        $bd = Account::find($r->account_id);
+        if (!$bd) {
+
+            return response()->json([
+                'success' => false,
+                'msg' => 'Error getting account details'
+            ]);
+        }
+
+        $acct_name = $bd->account_name;
+        $bank_name = $bd->bank_name;
+        $acct_num = $bd->account_number;
+        $bank_code = Bank::find($bd->bank_id)->code;
+        $tid = 3;
+
+        if ($bank_code != '090175') {
+            $charge = 200;
+        }
+
+        //Check daily limit
+        $today_total = Auth::user()->nairaTransactions()->whereDate('created_at', now())->whereIn('transaction_type_id', [3, 2])->sum('amount');
+        if ($today_total >= Auth::user()->daily_max) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Daily limit exceeded, please upgrade your account to continue.'
+            ]);
+        }
+
+        //check Monthly
+        $monthly_total = Auth::user()->nairaTransactions()->whereYear('created_at', now())->whereMonth('created_at', now())->whereIn('transaction_type_id', [3, 2])->sum('amount');
+        if ($monthly_total >= Auth::user()->monthly_max) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Monthly limit exceeded, please upgrade your account limits to continue.'
+            ]);
+        }
+
+
+        $n = Auth::user()->nairaWallet;
+
+        if (Hash::check($r->pin, $n->password) == false) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Wrong wallet pin entered'
+            ]);
+        }
+
+        $amount = $r->amount - $charge;
+        //$amount_paid = $r->amount;
+
+        if ($r->amount > $n->amount) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Insufficient funds'
+            ]);
+        }
+
+        if ($tid == 0) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Something went wrong, please try again'
+            ]);
+        }
+
+        $prev_bal = $n->amount;
+        $n->amount -= $r->amount;
+        $n->save();
+
+        $msg = 'Transaction initiated';
+        $nt = new NairaTransaction();
+        $nt->reference = $reference;
+        $nt->amount = $r->amount;
+
+        $nt->previous_balance = $prev_bal;
+        $nt->current_balance = $n->amount;
+        $nt->charge = $charge;
+        $nt->transfer_charge = 197;
+        $nt->sms_charge = 3;
+        if ($charge == 0) {
+            $nt->transfer_charge = 0; //overide the previous
+            $nt->sms_charge = 0;
+        }
+        $nt->amount_paid = $amount;
+        $nt->transaction_type_id = $tid;
+        $nt->user_id = Auth::user()->id;
+        $nt->type = 'naira wallet';
+        $nt->dr_user_id = Auth::user()->id;
+        $nt->dr_wallet_id = $n->id;
+        $nt->dr_acct_name = Auth::user()->first_name;
+        $nt->cr_acct_name = $acct_name . ' ' . $acct_num . " " . $bank_name;
+        $nt->narration = $r->narration;
+        $nt->trans_msg = $msg;
+        $nt->status = 'pending';
+        $nt->save();
+
+        /* Credit SMS and Transfer Wallet */
+        $transfer_charges_wallet = NairaWallet::where('account_number', 0000000001)->first();
+        $transfer_charges_wallet->amount += $nt->transfer_charge;
+        $transfer_charges_wallet->save();
+
+        $sms_charges_wallet = NairaWallet::where('account_number', 0000000002)->first();
+        $sms_charges_wallet->amount += $nt->sms_charge;
+        $sms_charges_wallet->save();
+
+        $title = 'Dantown wallet Debit';
+        $msg_body = 'Your Dantown wallet has been debited with N' . $amount . ' for transfer to ' . $nt->cr_acct_name . ' desc: ' . $r->narration;
+        $not = Notification::create([
+            'user_id' => Auth::user()->id,
+            'title' => $title,
+            'body' => $msg_body,
+        ]);
+        if (Auth::user()->notificationSetting->wallet_email == 1) {
+            // Mail::to(Auth::user()->email)->send(new WalletAlert($nt, 'debit'));
+        }
+
+        return response()->json([
+            'success' => true,
+            'msg' => 'Your withdrawal order has been placed, it will be processed in 3-4 hours'
+        ]);
     }
 }
