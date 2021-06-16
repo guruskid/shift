746f15e60ec81de657319534c376c81f17c2ae3f
sheanwinston <sheanwinston@gmail.com>
2021-06-16 19:41:46 +0100

    change

--- app/Http/Controllers/Admin/SummaryController.php
+++ app/Http/Controllers/Admin/SummaryController.php
@@ -61,7 +61,7 @@ class SummaryController extends Controller
             'end' => 'required|date|string',
         ]);
         //$transactions = Transaction::->paginate(200);
-        dd($data['start']);
+        //dd($data['start']);
         $sell_transactions = Transaction::where('card_id', 102)->where('type', 'sell')
         ->where('created_at', '>=', $data['start'])->where('created_at', '<=', $data['end'])
         ->where('status', 'success')->get();
