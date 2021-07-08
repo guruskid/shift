0c74c75ecd1822e26d1482b1c4cbb083ae44ea36
rickydchamp <richarduraih@gmail.com>
2021-07-08 14:33:43 +0100

    latest changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,7 @@ $countries = App\Country::orderBy('phonecode', 'asc')->get();
                     @endforeach
                     <div id="step_one">
                         <div class="form-group">
-                            <input type="text" name="first_name" required placeholder="Full Name" class="form-control col-11 mx-auto mx-md-0 col-lg-8">
+                            <input type="text" name="first_name" required placeholder="First Name" class="form-control col-11 mx-auto mx-md-0 col-lg-8">
                         </div>
 
                         <div class="form-group">
