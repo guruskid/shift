3eb3fa691bce0c0ff2889f21ed10e33ef3bdc2cf
rickydchamp <richarduraih@gmail.com>
2021-07-15 10:45:29 +0100

    My New Changes

--- app/Http/Controllers/Api/BillsPaymentController.php
+++ app/Http/Controllers/Api/BillsPaymentController.php
@@ -1,9 +1,26 @@
-public function nairaRate()
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\BitcoinTransaction;
+use App\Card;
+use App\CardCurrency;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\NairaTransaction;
+use App\Notification;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class BillsPaymentController extends Controller
+{
+    public function nairaRate()
     {
         $naira_wallet = Auth::user()->nairaWallet;
         $balance = $naira_wallet->amount;
         // dd($balance);
-
         $card = Card::find(102);
         $rates = $card->currency-> first();
 
@@ -109,7 +126,7 @@ public function nairaRate()
         if($request->amount > 25000){
             return response()->json([
                 'success' => false,
-                'message' => 'Minimium amount is ₦25000',
+                'message' => 'Maximium amount is ₦25000',
                 'response_description' => 'TRANSACTION FAILURE',
             ]);
         }
@@ -262,244 +279,246 @@ public function nairaRate()
 
 
 
-    public function bitcoinAirtime(Request $request)
-    {
-        $request->validate([
-            'network' => "required",
-            'reference' => 'required',
-            'amount' => "required",
-            'rechargetype' => 'string',
-            'password' => "required"
-        ]);
+    // public function bitcoinAirtime(Request $request)
+    // {
+    //     $request->validate([
+    //         'network' => "required",
+    //         'reference' => 'required',
+    //         'amount' => "required",
+    //         'rechargetype' => 'string',
+    //         'password' => "required"
+    //     ]);
 
 
-        if ($request->rechargetype == 'self') {
-            $phone = Auth::user()->country->phonecode . Auth::user()->phone;
-        } else{
-            $request->validate([
-                'phone' => 'required'
-            ]);
+    //     if ($request->rechargetype == 'self') {
+    //         $phone = Auth::user()->country->phonecode . Auth::user()->phone;
+    //     } else{
+    //         $request->validate([
+    //             'phone' => 'required'
+    //         ]);
 
-            $phone = $request->phone;
-        }
-
-
-        // dd($phone);
-
-        $card = Card::find(102);
-        $rates = $card->currency-> first();
-
-        $sell = CardCurrency::where([
-            'card_id' => 102,
-            'currency_id' => $rates->id,
-            'buy_sell' => 2])->first()->paymentMediums()->first();
-        $trade_rate = json_decode($sell->pivot->payment_range_settings);
-        //  dd($trade_rate);
+    //         $phone = $request->phone;
+    //     }
 
 
-        // dd($trade_rate);
+    //     // dd($phone);
 
-        // $amt_usd= $request->amount/$trade_rate;
+    //     $card = Card::find(102);
+    //     $rates = $card->currency-> first();
 
-        $amt_usd= $request->amount/$trade_rate[0]->rate;
-
-
-        $res = json_decode(file_get_contents("http://api.coinbase.com/v2/prices/spot?currency=USD"));
-        // dd($res);
-
-        $amt_btc = $amt_usd/$res->data->amount;
-
-        $bitcoin_wallet = Auth::user()->bitcoinWallet;
-        $balance = $bitcoin_wallet->balance;
-        $pin = $bitcoin_wallet->password;
-        $put_pin = $request->password;
-        $hash = Hash::check($put_pin, $pin);
-
-        if(!$hash)
-        {
-            return response()->json([
-                'success' => false,
-                'message' => 'Incorrect Pin',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
-
-        // dd($balance);
-
-        if($request->amount > $balance){
-            return response()->json([
-                'success' => false,
-                'message' => 'Insufficient balance',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
-
-        if($request->amount < 100){
-            return response()->json([
-                'success' => false,
-                'message' => 'Minimium amount is ₦100',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
-
-        if($request->amount > 25000){
-            return response()->json([
-                'success' => false,
-                'message' => 'Minimium amount is ₦25000',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
-
-        $priceDeduction = $balance - $amt_btc;
-        $new_balance = $bitcoin_wallet->update([
-            "balance" => $priceDeduction,
-        ]);
-
-        $bt = new BitcoinTransaction();
-        $bt->hash = $request->reference;
-        $bt->narration = $phone . ' ' . 'Payment for recharge card';
-        $bt->user_id = Auth::user()->id;
-        $bt->primary_wallet_id = 1;
-        $bt->wallet_id = $bitcoin_wallet->address;
-        $bt->previous_balance = $balance;
-        $bt->current_balance = $new_balance;
-        $bt->debit = $amt_btc;
-        $bt->fee = 0;
-        $bt->credit = 0;
-        $bt->charge = 0;
-        $bt->transaction_type_id = 9;
-        $bt->counterparty = $phone ;
-        $bt->confirmations = 3;
-        $bt->status = 'pending';
-        $bt->save();
-
-        $client = new Client((['auth' => ['dantownrec2@gmail.com', 'D@Nto99btc']]));
-        $url = "https://sandbox.vtpass.com/api/pay";
-        $response = $client->request('POST', $url, [
-            'json' => [
-                // 'request_id' => Str::random(6),
-                'request_id' => $request->reference,
-                'serviceID' => $request->network,
-                'amount' => $request->amount,
-                'phone' => $request->phone
-            ]
-        ]);
-        $body = json_decode($response->getBody()->getContents());
-        // dd($body);
+    //     $sell = CardCurrency::where([
+    //         'card_id' => 102,
+    //         'currency_id' => $rates->id,
+    //         'buy_sell' => 2])->first()->paymentMediums()->first();
+    //     $trade_rate = json_decode($sell->pivot->payment_range_settings);
+    //     //  dd($trade_rate);
 
 
-        if ($body->code == 000) {
-            $bt->status = 'success';
-            $bt->save();
-            // dd('success');
+    //     // dd($trade_rate);
 
-            $naira_charge = $body->content->transactions->commission;
+    //     // $amt_usd= $request->amount/$trade_rate;
 
-            $sell = CardCurrency::where([
-                'card_id' => 102,
-                'currency_id' => $rates->id,
-                'buy_sell' => 2])->first()->paymentMediums()->first();
-            $trade_rate = json_decode($sell->pivot->payment_range_settings);
-
-            $charge_usd= $naira_charge/$trade_rate[0]->rate;
-
-            $res = json_decode(file_get_contents("http://api.coinbase.com/v2/prices/spot?currency=USD"));
-
-            $bt->charge = $charge_usd/$res->data->amount;
-            $bt->save();
+    //     $amt_usd= $request->amount/$trade_rate[0]->rate;
 
 
+    //     $res = json_decode(file_get_contents("http://api.coinbase.com/v2/prices/spot?currency=USD"));
+    //     // dd($res);
 
-            $title = 'Recharge card purchase';
-            $msg_body = 'Your Dantown wallet has been debited with N' . $request->amount . ' for recharge card purchase';
+    //     $amt_btc = $amt_usd/$res->data->amount;
 
-            $not = Notification::create([
-                'user_id' => Auth::user()->id,
-                'title' => $title,
-                'body' => $msg_body,
-            ]);
+    //     $bitcoin_wallet = Auth::user()->bitcoinWallet;
+    //     $balance = $bitcoin_wallet->balance;
+    //     $pin = $bitcoin_wallet->password;
+    //     $put_pin = $request->password;
+    //     $hash = Hash::check($put_pin, $pin);
 
-            //  Mail::to(Auth::user()->email)->send(new DantownNotification($title, $msg_body));
+    //     if(!$hash)
+    //     {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Incorrect Pin',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
 
-            $token = env('SMS_TOKEN');
-            $to = Auth::user()->phone;
-            $sms_url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=' . $token . '&from=Dantown&to=' . $to . '&body=' . $msg_body . '&dnd=2';
-            $snd_sms = $client->request('GET', $sms_url);
+    //     // dd($balance);
 
-            return response()->json([
-                'success' => true,
-                'response_description' => 'TRANSACTION SUCCESSFUL',
-                'message' => 'Your recharge is successful'
-            ]);
-        }
+    //     if($request->amount > $balance){
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Insufficient balance',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
 
-        elseif($body->code == 016){
-            $bt->status ='failed';
-            $bt->save();
-            $new_balance = $bitcoin_wallet->update([
-                "balance" => $balance,
-            ]);
+    //     if($request->amount < 100){
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Minimium amount is ₦100',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Your recharge failed',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
+    //     if($request->amount > 25000){
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Minimium amount is ₦25000',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
 
-        elseif ($body->code == 021){
-            $bt->status ='failed';
-            $bt->save();
-            $new_balance = $bitcoin_wallet->update([
-                "balance" => $balance,
-            ]);
+    //     $priceDeduction = $balance - $amt_btc;
+    //     $new_balance = $bitcoin_wallet->update([
+    //         "balance" => $priceDeduction,
+    //     ]);
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Your account is locked',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
+    //     $bt = new BitcoinTransaction();
+    //     $bt->hash = $request->reference;
+    //     $bt->narration = $phone . ' ' . 'Payment for recharge card';
+    //     $bt->user_id = Auth::user()->id;
+    //     $bt->primary_wallet_id = 1;
+    //     $bt->wallet_id = $bitcoin_wallet->address;
+    //     $bt->previous_balance = $balance;
+    //     $bt->current_balance = $new_balance;
+    //     $bt->debit = $amt_btc;
+    //     $bt->fee = 0;
+    //     $bt->credit = 0;
+    //     $bt->charge = 0;
+    //     $bt->transaction_type_id = 9;
+    //     $bt->counterparty = $phone ;
+    //     $bt->confirmations = 3;
+    //     $bt->status = 'pending';
+    //     $bt->save();
 
-        elseif ($body->code == 022){
-            $bt->status ='failed';
-            $bt->save();
-            $new_balance = $bitcoin_wallet->update([
-                "balance" => $balance,
-            ]);
+    //     $client = new Client((['auth' => ['dantownrec2@gmail.com', 'D@Nto99btc']]));
+    //     $url = "https://sandbox.vtpass.com/api/pay";
+    //     $response = $client->request('POST', $url, [
+    //         'json' => [
+    //             // 'request_id' => Str::random(6),
+    //             'request_id' => $request->reference,
+    //             'serviceID' => $request->network,
+    //             'amount' => $request->amount,
+    //             'phone' => $request->phone
+    //         ]
+    //     ]);
+    //     $body = json_decode($response->getBody()->getContents());
+    //     // dd($body);
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Your account is suspended',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
 
-        elseif ($body->code == 024) {
-            $bt->status ='failed';
-            $bt->save();
-            $new_balance = $bitcoin_wallet->update([
-                "amount" => $balance,
-            ]);
+    //     if ($body->code == 000) {
+    //         $bt->status = 'success';
+    //         $bt->save();
+    //         // dd('success');
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Your account is inactive',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-         }
+    //         $naira_charge = $body->content->transactions->commission;
 
-        else{
-            $bt->status ='failed';
-            $bt->save();
-            $new_balance = $bitcoin_wallet->update([
-                "balance" => $balance,
-            ]);
+    //         $sell = CardCurrency::where([
+    //             'card_id' => 102,
+    //             'currency_id' => $rates->id,
+    //             'buy_sell' => 2])->first()->paymentMediums()->first();
+    //         $trade_rate = json_decode($sell->pivot->payment_range_settings);
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Please try again later',
-                'response_description' => 'TRANSACTION FAILURE',
-            ]);
-        }
-    }
+    //         $charge_usd= $naira_charge/$trade_rate[0]->rate;
+
+    //         $res = json_decode(file_get_contents("http://api.coinbase.com/v2/prices/spot?currency=USD"));
+
+    //         $bt->charge = $charge_usd/$res->data->amount;
+    //         $bt->save();
+
+
+
+    //         $title = 'Recharge card purchase';
+    //         $msg_body = 'Your Dantown wallet has been debited with N' . $request->amount . ' for recharge card purchase';
+
+    //         $not = Notification::create([
+    //             'user_id' => Auth::user()->id,
+    //             'title' => $title,
+    //             'body' => $msg_body,
+    //         ]);
+
+    //         //  Mail::to(Auth::user()->email)->send(new DantownNotification($title, $msg_body));
+
+    //         $token = env('SMS_TOKEN');
+    //         $to = Auth::user()->phone;
+    //         $sms_url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=' . $token . '&from=Dantown&to=' . $to . '&body=' . $msg_body . '&dnd=2';
+    //         $snd_sms = $client->request('GET', $sms_url);
+
+    //         return response()->json([
+    //             'success' => true,
+    //             'response_description' => 'TRANSACTION SUCCESSFUL',
+    //             'message' => 'Your recharge is successful'
+    //         ]);
+    //     }
+
+    //     elseif($body->code == 016){
+    //         $bt->status ='failed';
+    //         $bt->save();
+    //         $new_balance = $bitcoin_wallet->update([
+    //             "balance" => $balance,
+    //         ]);
+
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Your recharge failed',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
+
+    //     elseif ($body->code == 021){
+    //         $bt->status ='failed';
+    //         $bt->save();
+    //         $new_balance = $bitcoin_wallet->update([
+    //             "balance" => $balance,
+    //         ]);
+
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Your account is locked',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
+
+    //     elseif ($body->code == 022){
+    //         $bt->status ='failed';
+    //         $bt->save();
+    //         $new_balance = $bitcoin_wallet->update([
+    //             "balance" => $balance,
+    //         ]);
+
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Your account is suspended',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
+
+    //     elseif ($body->code == 024) {
+    //         $bt->status ='failed';
+    //         $bt->save();
+    //         $new_balance = $bitcoin_wallet->update([
+    //             "amount" => $balance,
+    //         ]);
+
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Your account is inactive',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //      }
+
+    //     else{
+    //         $bt->status ='failed';
+    //         $bt->save();
+    //         $new_balance = $bitcoin_wallet->update([
+    //             "balance" => $balance,
+    //         ]);
+
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Please try again later',
+    //             'response_description' => 'TRANSACTION FAILURE',
+    //         ]);
+    //     }
+    // }
+
+}
