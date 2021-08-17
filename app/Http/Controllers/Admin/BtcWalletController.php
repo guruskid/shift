ca14bb5db895366fffe67c8e723adac596b1fa4e
Winston <sheanwinston@gmail.com>
2021-08-17 13:22:34 +0100

    Pin or txns

--- app/Http/Controllers/Admin/BtcWalletController.php
+++ app/Http/Controllers/Admin/BtcWalletController.php
@@ -173,9 +173,9 @@ class BtcWalletController extends Controller
 
         ]);
 
-       /*  if (!Hash::check($data['pin'], Auth::user()->pin)) {
+        if (!Hash::check($data['pin'], Auth::user()->pin)) {
             return back()->with(['error' => 'Incorrect wallet pin']);
-        } */
+        }
 
         if ($request->wallet == 'hd') {
             $wallet = HdWallet::where('currency_id', 1)->first();
