13a08d8dc0c0c36332991f3a150a228ae7418ef2
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-12-10 15:47:23 +0100

    fixes

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -38,6 +38,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/view-Faq/{id}', 'FaqApiController@getFaq');
 
     //?TicketCategory
+    Route::get('/add-new', 'TicketController@addCategory');
     Route::get('/all-categories', "TicketCategoryController@listofCategories");
 
     //?ticket
