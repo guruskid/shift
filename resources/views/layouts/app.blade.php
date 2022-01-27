f3c294d96fa0115154cc5897ffe9548ac3a10e82
raymondj9 <raymondobeje@gmail.com>
2022-01-27 14:37:01 +0100

    update tranx error on chinese dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -291,7 +291,7 @@ $not = $nots->last();
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     @auth
-    @if (in_array(Auth::user()->role, [999, 889, 888, 777, 666] ))
+    @if (in_array(Auth::user()->role, [999, 889, 888, 777, 666, 444] ))
     <script src="{{asset('js/sa.js?v=7')}}"></script>
     @endif
     @endauth
