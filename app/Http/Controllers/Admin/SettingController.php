5db5521b6bbff1bc18adafce183160e30a02b602
winston <sheanwinston@gmail.com>
2022-03-12 10:18:16 +0100

    dev fixes

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -10,14 +10,18 @@ class SettingController extends Controller
 {
     public static function get($name)
     {
-        return Setting::where('name', $name)->first()->value;
+        return Setting::where('name', $name)->first()->value ?? 0;
     }
 
     public static function set(Request $request)
     {
-        $setting = Setting::where('name', $request->name)->first();
-        $setting->value = $request->value;
-        $setting->save();
+        // $setting = Setting::where('name', $request->name)->first();
+        Setting::updateOrCreate(
+            ['name' => $request->name],
+            ['value' => $request->value]
+        );
+        // $setting->value = $request->value;
+        // $setting->save();
 
         return back()->with(['success' => 'Settings updated']);
     }
