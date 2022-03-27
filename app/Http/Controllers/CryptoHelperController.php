75ebf3c37e79a16e3f4f54db8ef81120e848c2b2
winston <sheanwinston@gmail.com>
2022-03-27 20:36:41 +0100

    fixes

--- app/Http/Controllers/CryptoHelperController.php
+++ app/Http/Controllers/CryptoHelperController.php
@@ -41,56 +41,56 @@ class CryptoHelperController extends Controller
         }
     }
 
-    public static function balance($currency_id) //User Balance
-    {
-        $user = Auth::user();
+    // public static function balance($currency_id) //User Balance
+    // {
+    //     $user = Auth::user();
 
-        $wallet = '';
-        $usd = 0;
-        $ngn = 0;
-        switch ($currency_id) {
-            case 0:
-                $wallet = $user->nairaWallet;
-                return $wallet;
-                break;
-            case 1:
-                $wallet = $user->btcWallet;
-                $usd = LiveRateController::btcRate();
-                $ngn = LiveRateController::usdNgn();
-                break;
-            case 5:
-                $wallet = $user->tronWallet;
-                $usd = LiveRateController::tronRate();
-                $ngn = LiveRateController::usdNgn();
-                break;
-            case 7:
-                $wallet = $user->usdtWallet;
-                $usd = LiveRateController::usdtRate();
-                $ngn = CryptoRate::where(['type' => 'sell', 'crypto_currency_id' => 7])->first()->rate;
-                break;
+    //     $wallet = '';
+    //     $usd = 0;
+    //     $ngn = 0;
+    //     switch ($currency_id) {
+    //         case 0:
+    //             $wallet = $user->nairaWallet;
+    //             return $wallet;
+    //             break;
+    //         case 1:
+    //             $wallet = $user->btcWallet;
+    //             $usd = LiveRateController::btcRate();
+    //             $ngn = LiveRateController::usdNgn();
+    //             break;
+    //         case 5:
+    //             $wallet = $user->tronWallet;
+    //             $usd = LiveRateController::tronRate();
+    //             $ngn = LiveRateController::usdNgn();
+    //             break;
+    //         case 7:
+    //             $wallet = $user->usdtWallet;
+    //             $usd = LiveRateController::usdtRate();
+    //             $ngn = CryptoRate::where(['type' => 'sell', 'crypto_currency_id' => 7])->first()->rate;
+    //             break;
 
-            default:
-                # code...
-                break;
-        }
+    //         default:
+    //             # code...
+    //             break;
+    //     }
 
 
-        if ($wallet) {
-            $client = new Client();
-            $url = env('TATUM_URL') . '/ledger/account/' . $wallet->account_id;
-            $res = $client->request('GET', $url, [
-                'headers' => ['x-api-key' => env('TATUM_KEY')]
-            ]);
+    //     if ($wallet) {
+    //         $client = new Client();
+    //         $url = env('TATUM_URL') . '/ledger/account/' . $wallet->account_id;
+    //         $res = $client->request('GET', $url, [
+    //             'headers' => ['x-api-key' => env('TATUM_KEY')]
+    //         ]);
 
-            $accounts = json_decode($res->getBody());
+    //         $accounts = json_decode($res->getBody());
 
-            $wallet->balance = $accounts->balance->availableBalance;
-            $wallet->usd = number_format(((float)$wallet->balance * (float)$usd), 2);
-            $wallet->ngn = $wallet->usd * $ngn;
-        }
+    //         $wallet->balance = $accounts->balance->availableBalance;
+    //         $wallet->usd = number_format(((float)$wallet->balance * (float)$usd), 2);
+    //         $wallet->ngn = $wallet->usd * $ngn;
+    //     }
 
-        return $wallet;
-    }
+    //     return $wallet;
+    // }
 
     public static function balance($currency_id) //User Balance
     {
