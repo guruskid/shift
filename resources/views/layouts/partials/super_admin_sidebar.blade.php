a6b3099ad71ac9a159794e9ff0da7f2a3888fdff
barituleera <barituleera@gmail.com>
2022-04-29 16:02:21 +0100

    migrating limit fixes from manager to super admin

--- resources/views/layouts/partials/super_admin_sidebar.blade.php
+++ resources/views/layouts/partials/super_admin_sidebar.blade.php
@@ -150,6 +150,15 @@
         </a>
     </li>
 
+    <li class="menu {{ Route::currentRouteName() == 'admin.verification_limit' ? 'active' : '' }}">
+        <a href="{{route('admin.verification_limit')}}" aria-expanded="false" class="dropdown-toggle">
+            <div class="">
+                <ion-icon name="people-circle-outline"></ion-icon>
+                <span>Verification limit</span>
+            </div>
+        </a>
+    </li>
+
     <li class="menu {{ Route::currentRouteName() == 'admin.cards' ? 'active' : '' }}">
         <a href="{{route('admin.cards')}}" aria-expanded="false" class="dropdown-toggle">
             <div class="">
