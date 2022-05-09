03d1cad380690dccddae2fdad3f850e89265d266
barituleera <barituleera@gmail.com>
2022-05-09 06:42:26 +0100

    verification

--- app/Http/Controllers/ApiV2/Admin/AdminController.php
+++ app/Http/Controllers/ApiV2/Admin/AdminController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\ApiV2\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Mail\GeneralTemplateOne;
 use App\NairaTrade;
+use App\Notification;
 use App\User;
+use App\Verification;
+use App\VerificationLimit;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
@@ -126,6 +131,93 @@ class AdminController extends Controller
             'success' => true,
             'accountant' => User::where('role', [889, 777])->latest()->get(),
         ]);
+    }
+
+    public function allVerification()
+    {
+        $verifications = Verification::where('status', 'waiting')->latest()->get();
+
+        return response()->json([
+            'success' => true,
+            'verifications' => $verifications,
+        ]);
+    }
+
+    public function verifyUser(Verification $verification)
+    {
+        if ($verification->type == 'ID Card') {
+            $verification->user->idcard_verified_at = now();
+            $level = VerificationLimit::where('level', "3")->first();
+            $title = 'LEVEL 3 VERIFICATION SUCCESSFUL';
+            $body = "Congrats " . $verification->user->first_name . ", you have successfully completed your Level 3 verification.
+            Below is a breakdown of level 3 privileges. <br><br>
+
+            <b style='color:000070'>Identity Verification<br><br>
+
+            Daily withdrawal limit: NGN ".number_format($level->daily_widthdrawal_limit)."<br><br>
+
+            Monthly withdrawal limit: NGN ".number_format($level->monthly_widthdrawal_limit)."<br><br>
+
+            Crypto withdrawal limit: ".$level->crypto_widthdrawal_limit."<br><br>
+
+            Crypto deposit: ".$level->crypto_deposit."<br><br>
+
+            Transactions: ".$level->transactions."<br></b>
+            ";
+
+            $btn_text = '';
+            $btn_url = '';
+
+            $name = ($verification->user->first_name == " ") ? $verification->user->username : $verification->user->first_name;
+            $name = str_replace(' ', '', $name);
+            $firstname = ucfirst($name);
+            Mail::to($verification->user->email)->send(new GeneralTemplateOne($title, $body, $btn_text, $btn_url, $firstname));
+        } elseif ($verification->type == 'Address') {
+            $verification->user->address_verified_at = now();
+            $level = VerificationLimit::where('level', "2")->first();
+            $title = 'LEVEL 2 VERIFICATION SUCCESSFUL';
+            $body = "Congrats " . $verification->user->first_name . ", you have successfully completed your Level 2 verification.
+            Below is a breakdown of level 2 privileges. <br><br>
+
+            <b style='color:000070'>Address Verification<br><br>
+
+            Daily withdrawal limit: NGN ".number_format($level->daily_widthdrawal_limit)."<br><br>
+
+            Monthly withdrawal limit: NGN ".number_format($level->monthly_widthdrawal_limit)."<br><br>
+
+            Crypto withdrawal limit: ".$level->crypto_widthdrawal_limit."<br><br>
+
+            Crypto deposit: ".$level->crypto_deposit."<br><br>
+
+            Transactions: ".$level->transactions."<br></b>
+            ";
+
+            $btn_text = '';
+            $btn_url = '';
+
+            $name = ($verification->user->first_name == " ") ? $verification->user->username : $verification->user->first_name;
+            $name = str_replace(' ', '', $name);
+            $firstname = ucfirst($name);
+            Mail::to($verification->user->email)->send(new GeneralTemplateOne($title, $body, $btn_text, $btn_url, $firstname));
+        }
+
+        $verification->user->save();
+        $verification->status = 'success';
+        $verification->save();
+
+        Notification::create([
+            'user_id' => $verification->user->id,
+            'title' => 'Verification successful',
+            'body' => 'Congratulations, your ' . $verification->type . ' has been verified',
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User verified',
+        ]);
+
+        // back()->with(['success' => 'User verified']);
+
     }
 }
 
--- routes/api_admin.php
+++ routes/api_admin.php
@@ -36,5 +36,12 @@ Route::group(['middleware' => ['auth:api', 'verified', 'super']], function () {
     });
 
 
+    // Verification
+    Route::group(['prefix' => 'verification'], function () {
+        Route::GET('/get-all-verifications',  'AdminController@allVerification');
+        Route::put('/user-verification/{verification}',  'AdminController@verifyUser');
+    //     Route::POST('/edit',  'VerificationController@editVerification');
+    //     Route::POST('/delete',  'VerificationController@deleteVerification');
+    });
 
 });
