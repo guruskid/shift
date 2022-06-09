fc6d8df4bbb2ee334e67885329037dd975a6267f
winston <sheanwinston@gmail.com>
2022-06-09 09:04:14 +0100

    fixes

--- app/Http/Controllers/CryptoHelperController.php
+++ app/Http/Controllers/CryptoHelperController.php
@@ -88,7 +88,7 @@ class CryptoHelperController extends Controller
             $wallet->balance = $accounts->balance->availableBalance;
             $wallet->usd = $wallet->balance * (float)$usd;
             $wallet->ngn = $wallet->usd * $ngn;
-            $wallet->usd = (float)$wallet->usd;
+            $wallet->usd = round((float)$wallet->usd, 2);
         }
 
         return $wallet;
