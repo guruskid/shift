2e3a08e3f635ad2195153c20c22732c5cc9213cb
Wisdom <thewisdomokechukwu@gmail.com>
2022-06-24 15:41:54 +0100

    transactions details fixes and sort

--- app/Http/Controllers/ApiV2/Admin/SummaryController.php
+++ app/Http/Controllers/ApiV2/Admin/SummaryController.php
@@ -202,7 +202,6 @@ class SummaryController extends Controller
     public function sortGiftCardTransactions(Request $r)
     {
         $date = ($r->date == null) ? date('Y-m-d') : $r->date;
-
         if($r->token_id != null){
             $number_of_tranx = Transaction::where('card_id',$r->token_id)
             ->whereDate("created_at",">=",$date)->whereDate("created_at","<=",$date)->where('status', 'success')->get();
@@ -225,85 +224,199 @@ class SummaryController extends Controller
             'crypto_tokens' => $crypto_tokens,
             'transactions' => $number_of_tranx->paginate(10)
         ], 200);
-    }
+    }  
 
-    public function transactions($category,$date)
+    public function loadTransactionDetails()
     {
-        $total_number = ($date != null) ? 200 : 100;
-        $date = ($date != null) ? $date: Carbon::now();
-        $is_crypto = ($category == "Crypto") ? 1:0;
-        $data_collection = collect([]);
+        $date =now()->format('Y-m-d');
+        $total_number = 100;
+        $data_collection =  collect([]);
+
         for ($i=0; $i <= $total_number; $i++) { 
             $highest_naira_user_name = null;
             $highest_freq_user_name = null;
-            $dates = Carbon::parse($date)->subDays($i)->format("Y-m-d");
-            
-            $transactions =  Transaction::whereDate('created_at',$dates)->where('status', 'success');
-            
-            if($category != null){
-                $transactions = $transactions->whereHas('asset', function($query) use ($is_crypto){
-                    $query->where('is_crypto', $is_crypto);
-                });
-            }
-            
-            $transactions = $transactions->get();
-            
-            $successful_tranx =  $transactions->where('status', 'success')->count();
-            $declined_tranx = $transactions->where('status','!=', 'success')->where('status','!=', 'waiting')->count();
 
-            $highest_naira_user =Transaction::select('*', DB::raw('sum(amount_paid) AS total_amount'))
-            ->whereDate('created_at',$dates)->where('status', 'success');
-            if($category){
-                $highest_naira_user = $highest_naira_user->whereHas('asset', function($query) use ($is_crypto){
-                    $query->where('is_crypto', $is_crypto);
-                });
-            }
-            $highest_naira_user = $highest_naira_user->groupBy('user_id')->orderBy(DB::raw('total_amount'), 'desc')->first();
-            
+            $dates = Carbon::parse($date)->subDays($i)->format("Y-m-d");
+
+            $transactions = Transaction::whereDate('created_at',$dates)->get();
+            $utility_transactions = UtilityTransaction::whereDate('created_at',$dates)->where('status', 'success')->get();
+
+            $success_transactions = $transactions->where('status', 'success')->count();
+            $declined_transactions = $transactions->whereIn('status',['failed','declined'])->count();
+
+            $utility_success = $utility_transactions->where('status', 'success')->count();
+            $utility_declined = $utility_transactions->whereNotIn('status',['success','pending'])->count();
+
+            $successful_tranx = $success_transactions + $utility_success;
+            $declined_tranx = $declined_transactions + $utility_declined;
+
+            $highest_naira_user =  Transaction::select('*', DB::raw('sum(amount_paid) AS total_amount'))
+            ->whereDate('created_at',$dates)->where('status', 'success')->groupBy('user_id')
+            ->orderBy(DB::raw('total_amount'), 'desc')->first();
             if(isset($highest_naira_user->user)){
                 $highest_naira_user_name = $highest_naira_user->user->first_name." ".$highest_naira_user->user->last_name;
             }
 
             $highest_freq_user = Transaction::select('*', DB::raw('count(user_id) AS count'))
-            ->whereDate('created_at',$dates)->where('status', 'success');
-            if($category){
-                $highest_freq_user = $highest_freq_user->whereHas('asset', function($query) use ($is_crypto){
-                    $query->where('is_crypto', $is_crypto);
-                });
-            }
-            $highest_freq_user = $highest_freq_user->groupBy('user_id')->orderBy(DB::raw('count'), 'desc')->first();
+            ->whereDate('created_at',$dates)->where('status', 'success')->groupBy('user_id')
+            ->orderBy(DB::raw('count'), 'desc')->first();
             if(isset($highest_freq_user->user)){
                 $highest_freq_user_name = $highest_freq_user->user->first_name." ".$highest_freq_user->user->last_name;
             }
-            
 
             $successful_tranx_buy =  $transactions->where('status', 'success')->where('type','buy')->count();
             $successful_tranx_sell =  $transactions->where('status', 'success')->where('type','sell')->count();
-
             $naira_trades = NairaTrade::whereDate('created_at',$date)->where('status', 'success')->count();
-            
-                $data = array($i=>[
-                    'successful_transaction' => $successful_tranx,
-                    'declined_transaction' =>$declined_tranx,
-                    'highest_naira_user' => $highest_naira_user_name,
-                    'highest_freq_user' => $highest_freq_user_name,
-                    'successful_tranx_buy' => $successful_tranx_buy,
-                    'successful_tranx_sell' =>$successful_tranx_sell,
-                    'internal_transfer' => $naira_trades,
-                    'date' => $dates
-                ]);
 
-                $data_collection = $data_collection->concat($data);
-            
+            $data = array($i=>[
+                'successful_transaction' => $successful_tranx,
+                'declined_transaction' =>$declined_tranx,
+                'highest_naira_user' => $highest_naira_user_name,
+                'highest_freq_user' => $highest_freq_user_name,
+                'successful_tranx_buy' => $successful_tranx_buy,
+                'successful_tranx_sell' =>$successful_tranx_sell,
+                'internal_transfer' => $naira_trades,
+                'date' => $dates
+            ]);
+            $data_collection = $data_collection->concat($data);
         }
         return $data_collection->sortByDesc('date');
-
-
     }
 
+    public function TransactionsTD($date, $type, $category ,$is_crypto)
+    {
+        $total_number = 100;
+        $data_collection =  collect([]);
+
+        for ($i=0; $i <= $total_number; $i++) { 
+            $highest_naira_user_name = null;
+            $highest_freq_user_name = null;
+
+            $dates = Carbon::parse($date)->subDays($i)->format("Y-m-d");
+
+            $transactions = Transaction::whereDate('created_at',$dates);
+            if($category)
+            {
+                $transactions = $transactions->whereHas('asset', function($query) use ($is_crypto){
+                    $query->where('is_crypto',$is_crypto);
+                });
+            }
+            if($type)
+            {
+                $transactions = $transactions->where('type',$type);
+            }
+            $transactions = $transactions->get();
+
+            $successful_tranx = $transactions->where('status', 'success')->count();
+            $declined_tranx = $transactions->whereIn('status',['failed','declined'])->count();
+
+            $highest_naira_user =  Transaction::select('*', DB::raw('sum(amount_paid) AS total_amount'));
+            if($category)
+            {
+                $highest_naira_user = $highest_naira_user->whereHas('asset', function($query) use ($is_crypto){
+                    $query->where('is_crypto',$is_crypto);
+                });
+            }
+            if($type)
+            {
+                $highest_naira_user = $highest_naira_user->where('type',$type);
+            }
+            $highest_naira_user = $highest_naira_user->whereDate('created_at',$dates)->where('status', 'success')->groupBy('user_id')
+            ->orderBy(DB::raw('total_amount'), 'desc')->first();
+
+            if(isset($highest_naira_user->user)){
+                $highest_naira_user_name = $highest_naira_user->user->first_name." ".$highest_naira_user->user->last_name;
+            }
+
+            $highest_freq_user = Transaction::select('*', DB::raw('count(user_id) AS count'));
+            if($category);
+            {
+                $highest_freq_user = $highest_freq_user->whereHas('asset', function($query) use ($is_crypto){
+                    $query->where('is_crypto',$is_crypto);
+                });
+            }
+            if($type)
+            {
+                $highest_freq_user = $highest_freq_user->where('type',$type);
+            }
+            $highest_freq_user = $highest_freq_user->whereDate('created_at',$dates)->where('status', 'success')->groupBy('user_id')
+            ->orderBy(DB::raw('count'), 'desc')->first();
+
+            if(isset($highest_freq_user->user)){
+                $highest_freq_user_name = $highest_freq_user->user->first_name." ".$highest_freq_user->user->last_name;
+            }
+
+            $successful_tranx_buy =  $transactions->where('status', 'success')->where('type','buy')->count();
+            $successful_tranx_sell =  $transactions->where('status', 'success')->where('type','sell')->count();
+            $naira_trades = NairaTrade::whereDate('created_at',$date)->where('status', 'success')->count();
+
+            $data = array($i=>[
+                'successful_transaction' => $successful_tranx,
+                'declined_transaction' =>$declined_tranx,
+                'highest_naira_user' => $highest_naira_user_name,
+                'highest_freq_user' => $highest_freq_user_name,
+                'successful_tranx_buy' => $successful_tranx_buy,
+                'successful_tranx_sell' =>$successful_tranx_sell,
+                'internal_transfer' => $naira_trades,
+                'date' => $dates
+            ]);
+            $data_collection = $data_collection->concat($data);
+        }
+        return $data_collection->sortByDesc('date');
+    }
+
+    public function otherTransactionsTD($date, $type)
+    {
+        $total_number = 100;
+        $data_collection =  collect([]);
+
+        for ($i=0; $i <= $total_number; $i++) { 
+            $highest_naira_user_name = null;
+            $highest_freq_user_name = null;
+
+            $dates = Carbon::parse($date)->subDays($i)->format("Y-m-d");
+
+            $transactions = UtilityTransaction::whereDate('created_at',$dates)->get();
+
+            $successful_tranx = $transactions->where('status', 'success')->count();
+            $declined_tranx = $transactions->whereNotIn('status',['success','pending'])->count();
+
+            $highest_naira_user =  UtilityTransaction::select('*', DB::raw('sum(amount) AS total_amount'))
+            ->whereDate('created_at',$dates)->where('status', 'success')->groupBy('user_id')
+            ->orderBy(DB::raw('total_amount'), 'desc')->first();
+
+            if(isset($highest_naira_user->user)){
+                $highest_naira_user_name = $highest_naira_user->user->first_name." ".$highest_naira_user->user->last_name;
+            }
+
+            $highest_freq_user = UtilityTransaction::select('*', DB::raw('count(user_id) AS count'))
+            ->whereDate('created_at',$dates)->where('status', 'success')->groupBy('user_id')
+            ->orderBy(DB::raw('count'), 'desc')->first();
+
+            if(isset($highest_freq_user->user)){
+                $highest_freq_user_name = $highest_freq_user->user->first_name." ".$highest_freq_user->user->last_name;
+            }
+            $successful_tranx_buy =  0;
+            $successful_tranx_sell =  0;
+            $naira_trades = NairaTrade::whereDate('created_at',$date)->where('status', 'success')->count();
+
+            $data = array($i=>[
+                'successful_transaction' => $successful_tranx,
+                'declined_transaction' =>$declined_tranx,
+                'highest_naira_user' => $highest_naira_user_name,
+                'highest_freq_user' => $highest_freq_user_name,
+                'successful_tranx_buy' => $successful_tranx_buy,
+                'successful_tranx_sell' =>$successful_tranx_sell,
+                'internal_transfer' => $naira_trades,
+                'date' => $dates
+            ]);
+            $data_collection = $data_collection->concat($data);
+        }
+        return $data_collection->sortByDesc('date');
+    }
     public function transactionsDetails()
     {
-       $data_collection = $this->transactions(null,null);
+       $data_collection = $this->loadTransactionDetails();
         return response()->json([
             'success' => true,
             'data' => $data_collection->paginate(10),
@@ -313,7 +426,19 @@ class SummaryController extends Controller
 
     public function sortTransaction(Request $r)
     {
-        $data_collection = $this->transactions($r->category,$r->date);
+        $r->category = strtolower($r->category);
+        if($r->category == "crypto")
+        {
+            $data_collection =  $this->TransactionsTD($r->date, $r->type, $r->category, 1);
+        }
+        if($r->category == "giftcards")
+        {
+            $data_collection =  $this->TransactionsTD($r->date, $r->type ,$r->category, 0);
+        }
+        if($r->category == "others")
+        {
+            $data_collection =  $this->otherTransactionsTD($r->date, $r->type);
+        }
         return response()->json([
             'success' => true,
             'data' => $data_collection->paginate(10)
