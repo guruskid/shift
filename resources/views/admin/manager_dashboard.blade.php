dcbc014034e57f06c2ed470266ff73b34d3a7ee7
Wisdom <thewisdomokechukwu@gmail.com>
2022-08-23 16:51:12 +0100

    route error fix

--- resources/views/admin/manager_dashboard.blade.php
+++ resources/views/admin/manager_dashboard.blade.php
@@ -88,7 +88,7 @@
                                 </tbody>
                             </table>
                             <div class="View-all">
-                                <a href="{{route('admin.asset-transactions', 0)}}">View all </a>
+                                <a href="{{route('admin.assetsTransactions', 0)}}">View all </a>
                             </div>
                         </div>
                     </div>
@@ -171,7 +171,7 @@
                                 </tbody>
                             </table>
                             <div class="View-all">
-                                <a href="{{route('admin.asset-transactions', 1)}}">View all </a>
+                                <a href="{{route('admin.assetsTransactions', 1)}}">View all </a>
                             </div>
                         </div>
                     </div>
