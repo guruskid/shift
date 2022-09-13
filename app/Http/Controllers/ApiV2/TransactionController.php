e90be6972d80711637e4dada5c83c7e8dcf1fb64
Wisdom <thewisdomokechukwu@gmail.com>
2022-09-13 13:00:03 +0100

    fix fir all transaction

--- app/Http/Controllers/ApiV2/TransactionController.php
+++ app/Http/Controllers/ApiV2/TransactionController.php
@@ -61,15 +61,17 @@ class TransactionController extends Controller
     {
         $tranx = Auth::user()->transactions;
         $tranxData = CryptoGiftcardTransactionResource::collection($tranx);
+        $tranxData = collect($tranxData);
 
         $utilTranx = Auth::user()->utilityTransaction;
         $utilData = UtilityTransactionResource::collection($utilTranx);
+        $utilData = collect($utilData);
 
         $p2pTranx = Auth::user()->nairaTrades;
         $p2pData = NairaTradeResource::collection($p2pTranx);
+        $p2pData = collect($p2pData);
 
-        $allTranx = collect($tranxData,$utilData,$p2pData)
-        ->sortByDesc('updated_at');
+        $allTranx = collect()->concat($tranxData)->concat($utilData)->concat($p2pData)->sortByDesc('created_at');
 
         $transaction = array();
         foreach($allTranx as $at)
