a17f4b61d3f6d137034b4a34381226eb0d652894
Ogunlana Michael <56716012+MikeAdeO@users.noreply.github.com>
2022-10-17 22:14:39 +0100

    created blog categories migration and model

---
+++ app/BlogCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BlogCategory extends Model
+{
+    //
+    use SoftDeletes;
+    protected $fillable = ['title', "is_published"];
+    protected $hidden = ["created_at", "updated_at"];
+}
