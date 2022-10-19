b9c1821cf0bd053e3c29f7c6cc0b45fe9a4eceae
Ogunlana Michael <56716012+MikeAdeO@users.noreply.github.com>
2022-10-19 11:09:59 +0100

    fix [added slug to blog category and blog header]

--- app/BlogCategory.php
+++ app/BlogCategory.php
@@ -9,6 +9,22 @@ class BlogCategory extends Model
 {
     //
     use SoftDeletes;
-    protected $fillable = ['title', "is_published"];
+    protected $fillable = ['title', "is_published", "slug"];
     protected $hidden = ["created_at", "updated_at", "deleted_at"];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($category) {
+            // generate a slug base on the title
+          $slug =   $category->slug = Str::slug($category->title);
+
+            // check to see if any other slugs exist that are the same & count them
+        $count = static::whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")->count();
+
+        // if other slugs exist that are the same, append the count to the slug
+        $category->slug = $count ? "{$slug}-{$count}" : $slug;
+
+        });
+    }
 }
--- app/BlogHeading.php
+++ app/BlogHeading.php
@@ -8,6 +8,22 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BlogHeading extends Model
 {
     use SoftDeletes;
-    protected $fillable = ['title', "is_published"];
+    protected $fillable = ['title', "is_published", "slug"];
     protected $hidden = ["created_at", "updated_at", "deleted_at"];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($heading) {
+            // generate a slug base on the title
+          $slug =   $heading->slug = Str::slug($heading->title);
+
+            // check to see if any other slugs exist that are the same & count them
+        $count = static::whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")->count();
+
+        // if other slugs exist that are the same, append the count to the slug
+        $heading->slug = $count ? "{$slug}-{$count}" : $slug;
+
+        });
+    }
 }
