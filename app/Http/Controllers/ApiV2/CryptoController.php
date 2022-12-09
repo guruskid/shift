e22733a0d3aece2aa93deb822c4b401be7f469ce
Ogunlana Michael <56716012+MikeAdeO@users.noreply.github.com>
2022-12-09 16:17:13 +0100

    APP re-fix coin to Naira

--- app/Http/Controllers/ApiV2/CryptoController.php
+++ app/Http/Controllers/ApiV2/CryptoController.php
@@ -25,8 +25,8 @@ class CryptoController extends Controller
         $usdt = CryptoCurrency::find(7);
         $data['usdt_rate'] = LiveRateController::usdtRate();
         $data["btc_rate"] = LiveRateController::btcRate();
-        $data['usdt_rate_naira'] = LiveRateController::usdtNgn();
-        $data['btc_rate_naira'] = LiveRateController::btcNgn();
+        // $data['usdt_rate_naira'] = LiveRateController::usdtNgn();
+        // $data['btc_rate_naira'] = LiveRateController::btcNgn();
 
 
 
@@ -37,6 +37,7 @@ class CryptoController extends Controller
         $bitcoin->rates = [
             'send_charge' => $btc_rates->send_fee,
             'coin_to_usd' => $btc_rates->btc_to_usd,
+            'coin_to_ngn' => LiveRateController::btcNgn(),
             'usd_to_ngn' => LiveRateController::usdNgn(), // Similar to sell rate
             'buy_rate' => LiveRateController::usdNgn(true, 'buy'),
             'sell_rate' => LiveRateController::usdNgn(),
@@ -49,6 +50,7 @@ class CryptoController extends Controller
         $usdt->rates = [
             'send_charge' => Setting::where('name', 'usdt_send_charge')->first()->value,
             'coin_to_usd' => LiveRateController::usdtRate(),
+            'coin_to_ngn' => LiveRateController::usdtNgn(),
             'usd_to_ngn' => LiveRateController::usdNgn(),
             'buy_rate' => LiveRateController::usdNgn(true, 'buy'),
             'sell_rate' => LiveRateController::usdNgn(),
